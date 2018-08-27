031876be8d7ae221e22dc8e8d6259176c3c03775
Marcelo Pepis <marcelo.pepis@gmail.com>
2018-08-27 13:48:46 -0300

    Desafio de Criação de Interface em App's UWP.
    Interface criada com menu do tipo hamburguer, e navegação usando frames com duas paginas diferente.

---
+++ HamburguerHeavenChallenger/HamburguerHeavenChallenger/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace HamburguerHeavenChallenger
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyFrame.Navigate(typeof(Financial));
+        }
+
+        private void HamburguerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            if(lbi == FinancialListBoxItem)
+            {
+                MyFrame.Navigate(typeof(Financial));
+            }
+            else if(lbi == FoodListBoxItem)
+            {
+                MyFrame.Navigate(typeof(Food));
+            }
+        }
+
+        private void GoBack_Click(object sender, RoutedEventArgs e)
+        {
+            if(MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+            }
+        }
+    }
+}
