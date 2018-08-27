b8fc4f450fb845748fd6c8a8c1a679e493422912
Marcelo Pepis <marcelo.pepis@gmail.com>
2018-08-27 11:53:52 -0300

    Teste de Menu Tipo Hamburguer
    Teste de Menu Tipo Hamburguer usando splitview e recursos de fontes do windows como icones.

---
+++ HamburguerExample/HamburguerExample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace HamburguerExample
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburguerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ShareListBoxItem.IsSelected)
+            {
+                ResultTextBlock.Text = "Share";
+            }
+            else if(FavoritesListBoxItem.IsSelected)
+            {
+                ResultTextBlock.Text = "Favorites";
+            }
+        }
+    }
+}
