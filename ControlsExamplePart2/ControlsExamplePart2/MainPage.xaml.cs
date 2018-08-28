1eca88698820c37e99cf39672efffcf6eb2aa898
Marcelo Pepis <marcelo.pepis@gmail.com>
2018-08-28 20:19:56 -0300

    Exemplo de Controle em App's UWP Parte 2
    Diversos controles nativos de aplicativos UWP, calendarios, horas, barra de progresso, anel de progresso, slider, etc.

---
+++ ControlsExamplePart2/ControlsExamplePart2/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace ControlsExamplePart2
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string[] selectionItens = new string[] { "Ferdnand", "Frank", "Frida", "Nigel", "Tag", "Tanya", "Toddy", "Tanner", "Todd" };
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyCalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            var selectedDates = sender.SelectedDates.Select(p => p.Date.Month.ToString() + "/" + p.Date.Day.ToString() + "/" + p.Date.Year.ToString()).ToArray();
+            var values = string.Join(", ", selectedDates);
+            CalendarViewResultTextBlock.Text = values;
+        }
+
+        private void InnerFlyoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFlyout.Hide();
+        }
+
+        private void MyAutoSuggestionBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var autoSuggestBox = (AutoSuggestBox)sender;
+            var filtered = selectionItens.Where(p => p.StartsWith(autoSuggestBox.Text)).ToArray();
+            autoSuggestBox.ItemsSource = filtered;
+        }
+            
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyProgressRing.IsActive = false;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyProgressRing.IsActive = true;
+        }
+    }
+}
