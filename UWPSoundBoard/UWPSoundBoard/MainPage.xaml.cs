f1177cd7a1d307f06fdbeaa280c6cb4fd0bf4881
Marcelo Pepis <marcelo_pepis@outlook.com>
2018-09-18 10:46:42 -0300

    UWp Soundborard Build preliminas.
    Estrutura da main page pronta, data bind pronto.

---
+++ UWPSoundBoard/UWPSoundBoard/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPSoundBoard.Model;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace UWPSoundBoard
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Sound> Sounds;
+
+        private List<MenuItem> MenuItems;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Sounds = new ObservableCollection<Sound>();
+            SoundManager.GetAllSounds(Sounds);
+
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/animals.png", Category = SoundCategory.Animals });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/cartoon.png", Category = SoundCategory.Cartoons });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/taunt.png", Category = SoundCategory.Taunts });
+        }
+
+        private void HamburguerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
+        }
+
+        private void NavLinkList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void MenuItemListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UWPSoundBoard/UWPSoundBoard/Model/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public class SoundManager
+    {
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = GetSounds();
+            sounds.Clear();
+            allSounds.ForEach(p => sounds.Add(p));
+
+        }
+
+        public static void GetAllSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCategory)
+        {
+            var AllSounds = GetSounds();
+            var filteredSounds = AllSounds.Where(p => p.Category == soundCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+
+
+        private static List<Sound> GetSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+    }
+}
